b98e3bd47d42b5cc0f814e63d62ba1d3bd359488
gaus07 <gausnoori07@gmail.com>
2024-12-17 00:58:47 +0530

    04/12/2024

---
+++ Leetcode/Leetcode/src/Leetcode/Daily/December_2024/Dec24_2825.java
@@ -0,0 +1,20 @@
+package Leetcode.Daily.December_2024;
+
+public class Dec24_2825 {
+    static boolean canMakeSubsequence(String str1, String str2) {
+        int l = 0, r = 0;
+
+        while (l < str1.length() && r < str2.length()) {
+            if (str1.charAt(l) == str2.charAt(r) || str1.charAt(l) + 1 == str2.charAt(r) || str1.charAt(l) - 25 == str2.charAt(r)) {
+                r++;
+                if (r == str2.length()) return true;
+            }
+            l++;
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        String str1 = "abc", str2 = "ad";
+        System.out.println(canMakeSubsequence(str1, str2));
+    }
+}
