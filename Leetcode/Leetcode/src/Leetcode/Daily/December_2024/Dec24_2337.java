bd378816894939419768501fff87df6b9caec1e7
gaus07 <gausnoori07@gmail.com>
2024-12-17 23:09:47 +0530

    05/12/2024

---
+++ Leetcode/Leetcode/src/Leetcode/Daily/December_2024/Dec24_2337.java
@@ -0,0 +1,25 @@
+package Leetcode.Daily.December_2024;
+
+public class Dec24_2337 {
+    static boolean canChange(String start, String target) {
+        int n = start.length(), l = 0, r = 0;
+
+        while (l < n || r < n) {
+            while (l < n && start.charAt(l) == '_') l++;
+            while (r < n && target.charAt(r) == '_') r++;
+
+            if (l == n || r == n) return l == n && r == n;
+            if (start.charAt(l) != target.charAt(r)) return false;
+
+            if (start.charAt(l) == 'L' && l < r) return false;
+            if (start.charAt(l) == 'R' && l > r) return false;
+
+            l++; r++;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        String start = "_L__R__R_", target = "L______RR";
+        System.out.println(canChange(start, target));
+    }
+}
